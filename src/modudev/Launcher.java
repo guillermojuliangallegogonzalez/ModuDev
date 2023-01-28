6103ff16fd12c69b34d934e2788efe2321d05f61
Guillermo Gallego Gonzalez <114193449+guillermojuliangallegogonzalez@users.noreply.github.com>
2023-01-28 23:04:13 +0100

    Login

--- src/modudev/Launcher.java
+++ src/modudev/Launcher.java
@@ -4,27 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
  *
- * @author guillermogallegogonzalez
+ * @author guillermogallego
  */
-
-public class ModuDev extends Application {
+public class Launcher extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        
-        Parent root = FXMLLoader.load(getClass().getResource("SplashScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));        
         Scene scene = new Scene(root);
-        
-        stage.getIcons().add(new Image("/recursos/Icono.png"));
-        stage.setTitle("ModuDev");
-        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
